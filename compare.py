866fb3cccf76a70e82ee9cd82101a5de503c042b
Greg <glchristian@comcast.net>
2015-12-08 16:22:09 -0700

    firstcommit of compare.py

---
+++ compare.py
@@ -0,0 +1,11 @@
+# Greg Christian
+# Python 2.7.10
+#
+
+if eBay_price  <= Amazon_price:
+    pass
+else:
+    diff = eBay_price * .15
+    diff = eBay_price - diff
+    diff = diff - Amazon_price
+    
