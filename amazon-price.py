569a75a2873fc8b5e8e57a6bbef6c1bd2874e4cb
gregpuzzles1 <glchristian@comcast.net>
2015-12-26 09:39:13 -0700

    Dec 26

---
+++ amazon-price.py
@@ -0,0 +1,25 @@
+import webbrowser
+import os
+#import time
+
+new = 2
+
+url = "http://www.amazon.com/NFL-Seattle-Seahawks-Pillow-Pet/dp/B004V55SDE/ref \
+    =sr_1_1?ie=UTF8&qid=1450967484&sr=8-1&keywords=nfl+pillow+pet+seattle+seahawks"
+
+webbrowser.open(url, new=new)
+
+#time.sleep(10.0)
+os.system('pause')
+
+
+url = "http://www.amazon.com/NFL-Denver-Broncos-Pillow-Pet/dp/B004V4Z3XU/ref \
+    =sr_1_1?ie=UTF8&qid=1450967811&sr=8-1&keywords=nfl+pillow+pet+denver+broncos"
+
+webbrowser.open(url, new=new)
+
+os.system('pause')
+
+
+
+
