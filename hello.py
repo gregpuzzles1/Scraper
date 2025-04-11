cacd2ca2425b4a1f548a7269d1d15e0fe459c705
Greg Christian <gregpuzzles1@gmail.com>
2025-04-11 13:01:18 -0600

    hello.py added for fun

---
+++ hello.py
@@ -0,0 +1,9 @@
+# hello_greg.py
+
+def main():
+    print("Hello, world!")
+    print("This message brought to you by Greg Christian in Greeley, Colorado.")
+
+if __name__ == "__main__":
+    main()
+
